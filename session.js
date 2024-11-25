23de780360daf12e9de0f25466cd9ac370c537ed
Dishumd <dishumd984@gmail.com>
2024-11-25 10:40:36 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=OJcVkCbZ#5C_9G7zxqepWWPz9BSjejVm1-YhD0rVaAC05SI0sVVA",
 };
