6aa030912fec2f0fae99ed5d1297d6fa773470f2
Dishumd <dishumd984@gmail.com>
2024-11-25 21:21:04 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -24,5 +24,5 @@ module.exports = {
   "AUTO_NEWS": false,
   "OWNER_REACT": false,
   "EMOJI": "🏷", 
-  "SESSION_ID":"ANJU-MD=qF0T0A5C#A5lV24ep4d0vo_54A08vKhGcqlfUTXJy26cXhN-5Ebk"
+  "SESSION_ID":"ANJU-MD=rMtxGTJR#9QJ2wayLlaW_vrmhchWFYc_UliAi3HWz5272LfmjWck"
 };
